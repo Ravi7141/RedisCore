b1531600eed952d85d5a85a70e29c62b887f4b7c
Ravi Rajput <rajputravi2070@gmail.com>
2026-04-25 18:25:25 +0530

    base Structure

---
+++ src/test/java/com/example/redisguard/RedisGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.redisguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
